a5b14ebbc79e7eb9c750788bc4c38deb9768929d
nabefuta <satoshinabe@220@gmail.com>
2021-11-23 16:40:45 +0900

    changed setup

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-
+"""
+モジュールのセットアップを行う
+"""
 from setuptools import setup
 
 setup(
-    name="OJC",
-    version="alpha",
-    author='nabefuta220',
-    install_requires=[
-       "bs4","online-judge-api-client"
-    ],
-    entry_points={
-        "console_scripts": [
-        "acGUI = commands.main:main",
-        "runer = commands.runner:main"
-        ]
-        
-    }
+	name="OJC",
+	version="alpha",
+	author='nabefuta220',
+	install_requires=[
+	   "bs4","online-judge-api-client"
+	],
+	entry_points={
+		"console_scripts": [
+		"acGUI = commands.main:main",
+		"runer = commands.runner:main"
+		]
+
+	}
 )
