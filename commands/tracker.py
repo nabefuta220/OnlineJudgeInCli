79f40a782407a57ad448140d1c624396e10f2ab2
nabefuta <satoshinabe@220@gmail.com>
2021-11-24 18:41:05 +0900

    test

--- commands/tracker.py
+++ commands/tracker.py
@@ -112,3 +112,8 @@ def track(url:str, config_file:str, output_file:str):
         time.sleep(1)
     logger.info("terminate due to long time taken")
     return None
+def main2():
+    q=1
+    return q
+
+
