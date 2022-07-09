10ff961d1c9af26c6cb1c44794040bac421cadf2
nabefuta <satoshinabe@220@gmail.com>
2022-07-10 00:22:46 +0900

    変数を変更

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,6 @@
 
 from pathlib import Path
 
-THIS_MODULE = Path(__file__).parents[0].resolve()
+THIS_MODULE = Path(__file__).parent
 CONFIG_FILE = THIS_MODULE.parent / "config.json"
 LOGIN_URL = "https://atcoder.jp/login?continue="
